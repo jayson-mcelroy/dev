d45fba51cd8776221d13c53153b266b8b183f6b4
jayson-mcelroy <jayson.mcelroy@gmail.com>
2016-03-17 21:05:38 -0400

    app

--- db.php
+++ db.php
@@ -1,17 +1,38 @@
 <?php
 
+
 if(isset($_POST['saveRegister'])){
-  $date     = $_POST["date"];
-  $workout  = $_POST["workOut"];
-  $weight   = $_POST["weight"];
-  $reps     = $_POST["reps"];
-  $notes    = $_POST["notes"];
-  $myfile   = fopen("$date.txt", "a") or die("Unable to open file!");
+  $num1    = $_POST["date"];
+  $num2  = $_POST["workOut"];
+  $oper     = $_POST["operation"];
+
+  switch ($oper) {
+    case '+':
+      echo $num1 + $num2;
+      break;
+
+    case '-':
+      echo $num1 - $num2;
+      break;
+
+    case '*':
+      echo $num1 * $num2;
+      break;
+
+    case '/':
+      echo $num1 / $num2;
+      break;
+    default:
+      # code...
+      break;
+  }
+
+  /*$myfile   = fopen("$date.txt", "a") or die("Unable to open file!");
   fwrite($myfile, $workout);
   fwrite($myfile, $weight);
   fwrite($myfile, $reps);
   fwrite($myfile, $notes);
-  fclose($myfile);
+  fclose($myfile);*/
   }
 
 ?>
